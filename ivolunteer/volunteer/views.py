e2c5213102c70749046ecd08deee85f41da24b50
subhash <subhash_singh@criterionnetworks.com>
2014-11-10 00:59:07 +0530

    added the templating.

--- ivolunteer/volunteer/views.py
+++ ivolunteer/volunteer/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
 def volunteerview(request):
-    return HttpResponse('volunteer')
+    #return HttpResponse('volunteer')
+    return render_to_response('volun/base.html',{'author' : 'subh'})
