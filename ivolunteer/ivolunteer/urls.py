0630f89e1dfca43482a6991d56ffed3b9f5d2774
subhash <subhash_singh@criterionnetworks.com>
2014-11-10 01:59:06 +0530

    added the url for the documentation.

--- ivolunteer/ivolunteer/urls.py
+++ ivolunteer/ivolunteer/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'ivolunteer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # learn https://docs.djangoproject.com/en/dev/topics/http/urls/#captured-parameters
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^volunteer/$', volunteerview),
     url(r'^volunteername/(?P<name>\w+)/$', volunteer_name),
